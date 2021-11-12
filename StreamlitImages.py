f5745b61aa3a89885ca4d226c934c33c8de4162b
Natasha Gwena <87618190+NatashaGwena@users.noreply.github.com>
2021-11-12 13:25:56 +0300

    Update StreamlitImages.py

--- StreamlitImages.py
+++ StreamlitImages.py
@@ -23,17 +23,17 @@ with features:
     st.header('These are plots on the Respiratory Diseases within the dataset')
 
 
-    img1 = Image.open("Figure_1.png")
+    img1 = Image.open("https://github.com/NatashaGwena/Respiratory-Disease-Detection/blob/master/Figure_1.png")
     st.image(img1)
     st.markdown('* **Fig.1** Shows the Distribution of Respiratory Diseases within the dataset.')
     
 
-    img3 = Image.open("Figure_3.png")
+    img3 = Image.open("https://github.com/NatashaGwena/Respiratory-Disease-Detection/blob/master/Figure_3.png")
     st.image(img3)
     st.markdown('* **Fig.2** Shows the relationship between the rate of infections between both sexes.')
   
 
-    img5 = Image.open("Figure_5.png")
+    img5 = Image.open("https://github.com/NatashaGwena/Respiratory-Disease-Detection/blob/master/Figure_5.png")
     st.image(img5)
     st.markdown('* **Fig.3** Shows the distribution of infections per age.')
 
